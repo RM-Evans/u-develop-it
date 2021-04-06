74654019b8a6edda5b213cf61c972b303daf64f0
Jawsbreaker <evansrichardmichael@gmail.com>
2021-04-05 21:35:24 -0600

    add CRUD routes

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,10 +1,10 @@
 // utility to check if an object has the required properties
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
-module.exports = function(obj, ...props) {
+module.exports = function (obj, ...props) {
   const errors = [];
 
-  props.forEach((prop) => {
+  props.forEach(prop => {
     // if property is blank or doesn't exist, add to errors array
     if (obj[prop] === undefined || obj[prop] === '') {
       errors.push(`No ${prop} specified.`);
@@ -16,6 +16,6 @@ module.exports = function(obj, ...props) {
       error: errors.join(' ')
     };
   }
-  
+
   return null;
 };
