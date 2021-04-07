94a40ef0f3b6497c5af98274c1974ee01ae0d594
Jawsbreaker <evansrichardmichael@gmail.com>
2021-04-07 15:44:52 -0600

    insert voters

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE candidates (
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 -- foreign flag with CONSTRAINT
+
+
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
